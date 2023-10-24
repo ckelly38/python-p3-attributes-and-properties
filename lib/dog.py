0040b0c318ed16fcccf6a0e19fddd317380085a6
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-23 22:59:20 -0600

    this validates the input in the constructor by calling self.methodname(params)

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,25 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Dog", breed="Pug"):
+        self.setName(name);
+        self.setBreed(breed);
+
+    def setName(self, val):
+        if (val == None):
+            print("Name must be string between 1 and 25 characters.");
+        elif (type(val) == str and len(val) > 1 and len(val) < 25):
+            self.name = val;
+        else: print("Name must be string between 1 and 25 characters.");
+        return None;
+    
+    def getName(self):
+        return self.name;
+
+    def setBreed(self, val):
+        if (val not in APPROVED_BREEDS):
+            print("Breed must be in list of approved breeds.");
+        else: self.breed = val;
+        return None;
+
+    def getBreed(self): return self.breed;
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,24 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="name", job="ITC"):
+        self.setName(name);
+        self.setJob(job);
+    
+    def setName(self, val):
+        if (val == None):
+            print("Name must be string between 1 and 25 characters.");
+        elif (type(val) == str and len(val) > 1 and len(val) < 25):
+            self.name = val.title();
+        else: print("Name must be string between 1 and 25 characters.");
+    
+    def getName(self):
+        return self.name;
+
+    def setJob(self, val):
+        if (val == None or val not in APPROVED_JOBS):
+            print("Job must be in list of approved jobs.");
+        else: self.job = val;
+    
+    def getJob(self):
+        return self.job;
